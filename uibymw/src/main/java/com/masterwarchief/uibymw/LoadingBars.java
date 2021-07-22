75876917a70b5d23729bfec474f2ae6518bdc96b
santoshdvc <santoshdvc@gmail.com>
2021-07-22 19:33:11 +0530

    Update ongoing

--- uibymw/src/main/java/com/masterwarchief/uibymw/LoadingBars.java
+++ uibymw/src/main/java/com/masterwarchief/uibymw/LoadingBars.java
@@ -1,8 +1,13 @@
 package com.masterwarchief.uibymw;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class LoadingBars {
+public class LoadingBars extends View {
     public enum ShapeType {
         TRIANGLE,
         CIRCLE,
@@ -27,7 +32,8 @@ public class LoadingBars {
     private static final int DEFAULT_STROKE_COLOR = Color.TRANSPARENT;
     private static final float DEFAULT_BORDER_WIDTH = 0;
     private static final float DEFAULT_TITLE_STROKE_WIDTH = 0;
-    private static final int DEFAULT_WAVE_SHAPE = ShapeType.CIRCLE.ordinal();
+    private static final int DEFAULT_ANIMATION_TYPE= AnimationType.SIMPLE.ordinal();
+    private static final int DEFAULT_WAVE_SHAPE = ShapeType.RECTANGLE.ordinal();
     private static final int DEFAULT_ROUND_RECTANGLE_X_AND_Y = 30;
     private static final float DEFAULT_TITLE_TOP_SIZE = 18.0f;
     private static final float DEFAULT_TITLE_CENTER_SIZE = 22.0f;
@@ -43,12 +49,31 @@ public class LoadingBars {
     private int roundRectangleRadius;
 
     // Properties.
-    private String mTopTitle;
-    private String centerTitle;
-    private String mBottomTitle;
+    private String loadTitle;
     private float mDefaultWaterLevel;
     private float waterLevelRatio = 1f;
     private float waveShiftRatio = DEFAULT_WAVE_SHIFT_RATIO;
     private int progressValue = DEFAULT_WAVE_PROGRESS_VALUE;
-    private boolean mIsRoundRectangle;
+    private boolean roundRectangle;
+
+    // Animation.
+    private ObjectAnimator loadBarAnim;
+    private AnimatorSet animatorSet;
+
+    private Context mContext;
+
+    public LoadingBars(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LoadingBars(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+
+    }
+
+
 }
