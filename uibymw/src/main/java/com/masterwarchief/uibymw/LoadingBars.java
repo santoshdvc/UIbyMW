2dc22e68e7c20686648dcedb3c7441b2ecd7842b
santoshdvc <santoshdvc@gmail.com>
2021-07-23 20:25:28 +0530

    Fixed JUnit version

--- uibymw/src/main/java/com/masterwarchief/uibymw/LoadingBars.java
+++ uibymw/src/main/java/com/masterwarchief/uibymw/LoadingBars.java
@@ -6,9 +6,13 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
@@ -69,14 +73,14 @@ public class LoadingBars extends View {
     private int loadColor;
     private int shapeType;
     private int loadAnimType;
-    private int roundRectangleRadius;
+    private int roundRectangleRadius = 0;
+    private int roundRectangleXY;
     private float mAmplitudeRatio;
     private String loadTitle;
-    private float mDefaultWaterLevel;
+    private float defaultWaterLevel;
     private float waterLevelRatio = 1f;
     private float waveShiftRatio = DEFAULT_WAVE_SHIFT_RATIO;
     private int progressValue = DEFAULT_PROGRESS_VALUE;
-    private boolean roundRectangle;
 
     // Animation.
     private ObjectAnimator loadBarAnim;
@@ -117,6 +121,87 @@ public class LoadingBars extends View {
         setProgressValue(progressValue);
 
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvasSize = canvas.getWidth();
+        if (canvas.getHeight() < canvasSize) {
+            canvasSize = canvas.getHeight();
+        }
+        float borderWidth = loadBorderPaint.getStrokeWidth();
+
+        switch (shapeType) {
+            // Draw triangle
+            case 0:
+                // Currently does not support the border settings
+                Point start = new Point(0, getHeight());
+                Path triangle = getEquilateralTriangle(start, getWidth(), getHeight());
+                canvas.drawPath(triangle, loadBgPaint);
+                canvas.drawPath(triangle, loadWavePaint);
+                break;
+            // Draw circle
+            case 1:
+                if (borderWidth > 0) {
+                    canvas.drawCircle(getWidth() / 2f, getHeight() / 2f,
+                            (getWidth() - borderWidth) / 2f - 1f, loadBorderPaint);
+                }
+
+                float radius = getWidth() / 2f - borderWidth;
+                // Draw background
+                canvas.drawCircle(getWidth() / 2f, getHeight() / 2f, radius, loadBgPaint);
+                canvas.drawCircle(getWidth() / 2f, getHeight() / 2f, radius, loadWavePaint);
+                break;
+            // Draw square
+            case 2:
+                if (borderWidth > 0) {
+                    canvas.drawRect(
+                            borderWidth / 2f,
+                            borderWidth / 2f,
+                            getWidth() - borderWidth / 2f - 0.5f,
+                            getHeight() - borderWidth / 2f - 0.5f,
+                            loadBorderPaint);
+                }
+
+                canvas.drawRect(borderWidth, borderWidth, getWidth() - borderWidth,
+                        getHeight() - borderWidth, loadBgPaint);
+                canvas.drawRect(borderWidth, borderWidth, getWidth() - borderWidth,
+                        getHeight() - borderWidth, loadWavePaint);
+                break;
+            // Draw rectangle
+            case 3:
+                if (borderWidth > 0) {
+                    RectF rect = new RectF(borderWidth / 2f, borderWidth / 2f, getWidth() - borderWidth / 2f - 0.5f, getHeight() - borderWidth / 2f - 0.5f);
+                    canvas.drawRoundRect(rect, roundRectangleXY, roundRectangleXY, loadBorderPaint);
+                    canvas.drawRoundRect(rect, roundRectangleXY, roundRectangleXY, loadBgPaint);
+                    canvas.drawRoundRect(rect, roundRectangleXY, roundRectangleXY, loadWavePaint);
+                } else {
+                    RectF rect = new RectF(0, 0, getWidth(), getHeight());
+                    canvas.drawRoundRect(rect, roundRectangleXY, roundRectangleXY, loadBgPaint);
+                    canvas.drawRoundRect(rect, roundRectangleXY, roundRectangleXY, loadWavePaint);
+                }
+
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private Path getEquilateralTriangle(Point start, int width, int height) {
+        Point p2 = null, p3 = null;
+            p2 = new Point(start.x + width, start.y);
+            p3 = new Point(start.x + (width / 2), (int) (height - Math.sqrt(3.0) / 2 * height));
+        Path path = new Path();
+        path.moveTo(start.x, start.y);
+        path.lineTo(p2.x, p2.y);
+        path.lineTo(p3.x, p3.y);
+
+        return path;
+
+    }
+
+
     /**
      * @param progress Default to be 50.
      */
